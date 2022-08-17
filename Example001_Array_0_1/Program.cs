8fceba028d07dfbca939b0fbb5cf94203ebc98fe
Денис <denis070619870@rambler.ru>
2022-08-17 13:05:44 +0300

    Example001_Array_0_1

---
+++ Example001_Array_0_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int n = 8;
+int min = 0;
+int max = 1;
+
+int[] CreateArray(int n, int min, int max)
+{
+    int[] array1 = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array1[i] = new Random().Next(min, max +1);
+    }
+    return array1;
+}
+
+void PrintArray(int[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        System.Console.Write($"{array2[i]} ");
+    }
+}
+
+PrintArray(CreateArray(n, min, max));
