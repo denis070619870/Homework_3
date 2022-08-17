f8a859376ee399d8c39ba299e90ea8bc095ac9e8
Денис <denis070619870@rambler.ru>
2022-08-17 15:54:45 +0300

    Example006_QuantityFromSegment

---
+++ Example006_QuantityFromSegment/Program.cs
@@ -0,0 +1,30 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int size = 123;
+//int min = -100;
+//int max = 200;
+int count = 1;
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-100, 201);
+    }
+    return array;
+}
+
+void PrintCountArray(int[] array1)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i] + " ");
+        if (array1[i] >= 10 && array1[i] <= 99) count++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество чисел из диапазона [10, 99] равно {count}");
+    Console.WriteLine();
+}
+PrintCountArray(FillArray(size, -100, 201));
