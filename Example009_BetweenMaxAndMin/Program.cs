20c48ad6f7116f0229b6182c58f8378f13bbc174
Денис <denis070619870@rambler.ru>
2022-08-17 17:47:52 +0300

    Example009_BetweenMaxAndMin

---
+++ Example009_BetweenMaxAndMin/Program.cs
@@ -0,0 +1,30 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+int size = new Random().Next(1, 11);
+
+Console.WriteLine($"Количетсов элементов массива: {size}");
+
+double[] FillSourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.WriteLine(array1[i]);
+    }
+    double difference = array1.Max() - array1.Min();
+    Console.WriteLine($"Минимальное значение массива {array1.Min()}, максимальное значение массива {array1.Max()}");
+    Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    Console.WriteLine();
+}
+
+PrintDiffMinMax(FillSourceArray(size));
+Console.WriteLine();
