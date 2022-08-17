7d6a7e0d05a0d5c13026b5e5e5c4af5be1042167
Денис <denis070619870@rambler.ru>
2022-08-17 17:41:18 +0300

    Example008_ProductPairsNumber

---
+++ Example008_ProductPairsNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿// Найти произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int size = new Random().Next(10, 26);
+int min = 1;
+int max = 100;
+Console.WriteLine($"Элементов в массиве: {size}");
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] PairsNumber(int[] data)
+{
+    int[] product = new int[data.Length / 2];
+    int last = data.Length - 1;
+    for (int i = 0; i < data.Length / 2; i++)
+    {
+        product[i] = data[i] * data[last];
+        last = last - 1;
+    }
+    return product;
+}
+
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = FillArray(size, min, max);
+PrintArray(array);
+
+Console.WriteLine();
+PrintArray(PairsNumber(array));
